b23301534a374df7f2993fa1e246850eaa6ba5e4
anofeles <kaxam1@gmail.com>
2023-10-24 18:54:03 +0400

    edit html

--- jsqrcode-combined.js
+++ jsqrcode-combined.js
@@ -13,19 +13,6 @@ function onScanFailure(error) {
    console.log(error)
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 }
+    "qr-reader", { fps: 10, qrbox: 250 }, false
 );
-Html5Qrcode.getCameras().then(devices => {
-    /**
-     * devices would be an array of objects of type:
-     * { id: "id", label: "label" }
-     */
-    if (devices && devices.length) {
-        var cameraId = devices[0].id;
-        // .. use this to start scanning or just log devices to see
-        // which cameras are deteceted.
-    }
-}).catch(err => {
-    // handle err
-});
 html5QrcodeScanner.render(onScanSuccess,onScanFailure);
