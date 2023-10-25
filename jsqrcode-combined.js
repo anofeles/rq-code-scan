c0a2bc9ce83523634ea8b0ff82e9f89420052edf
anofeles <kaxam1@gmail.com>
2023-10-25 12:08:25 +0400

    edit html

--- jsqrcode-combined.js
+++ jsqrcode-combined.js
@@ -1,56 +1,31 @@
 // var resultContainer = document.getElementById('qr-reader-results');
+//
+// var lastResult, countResults = 0;
+// function onScanSuccess(decodedText, decodedResult) {
+//     if (decodedText !== lastResult) {
+//         ++countResults;
+//         lastResult = decodedText;
+//         // Handle on success condition with the decoded message.
+//         console.log(`Scan result ${decodedText}`, decodedResult);
+//     }
+// }
+// function onScanFailure(error) {
+//    console.log(error)
+// }
+// var html5QrcodeScanner = new Html5QrcodeScanner(
+//     "qr-reader", { fps: 10, qrbox: 250 }, false
+// );
+// html5QrcodeScanner.render(onScanSuccess,onScanFailure);
 
-var lastResult, countResults = 0;
-function onScanSuccess(decodedText, decodedResult) {
-    if (decodedText !== lastResult) {
-        ++countResults;
-        lastResult = decodedText;
-        // Handle on success condition with the decoded message.
-        console.log(`Scan result ${decodedText}`, decodedResult);
-    }
-}
-function onScanFailure(error) {
-   console.log(error)
-}
 
-Html5Qrcode.getCameras().then(devices => {
-    if (devices && devices.length) {
-        var cameraId;
-        var cameraLabel;
-        if (devices.length === 1) {
-            cameraId = devices[0].id;
-        } else {
-            cameraId = devices[1].id;
-            //This is for cellphones with 4 cameras. Usually the first 2 detected are the front so in my case selecting the third in the list worked.
-            if (cameraLabel.includes("front")) {
-                cameraId = devices[2].id;
-            }
-        }
+const html5QrCode = new Html5Qrcode("qr-reader");
 
-        const html5QrCode = new Html5Qrcode("qr-reader");
-        html5QrCode.start(
-            cameraId,
-            {
-                fps: 10,
-                qrbox: 250
-            },
-            qrCodeMessage => {
-                //Things you want to do when you match a QR Code
-            },
-            errorMessage => {
-                // parse error, ignore it.
-            })
-            .catch(err => {
-                // Start failed, handle it.
-            });
+// if you scanned , it will be write in clear text in your input field which in my case 'result'
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    alert(decodedText)
+    // document.getElementById('qr-reader-results').value = decodedText;
+};
+const config = { fps: 200, qrbox: 100 };
 
-    }
-}).catch(err => {
-
-});
-
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 }, false
-);
-
-html5QrcodeScanner.render(Html5Qrcode,onScanSuccess,onScanFailure);
+// prefer the back camera else the front one
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
