43aaf2086c94a5b1fa9a955fc4fc9beaa8572c73
anofeles <kaxam1@gmail.com>
2023-10-25 11:56:43 +0400

    edit html

--- jsqrcode-combined.js
+++ jsqrcode-combined.js
@@ -1,4 +1,4 @@
-var resultContainer = document.getElementById('qr-reader-results');
+// var resultContainer = document.getElementById('qr-reader-results');
 
 var lastResult, countResults = 0;
 function onScanSuccess(decodedText, decodedResult) {
@@ -12,45 +12,45 @@ function onScanSuccess(decodedText, decodedResult) {
 function onScanFailure(error) {
    console.log(error)
 }
+
+Html5Qrcode.getCameras().then(devices => {
+    if (devices && devices.length) {
+        var cameraId;
+        var cameraLabel;
+        if (devices.length === 1) {
+            cameraId = devices[0].id;
+        } else {
+            cameraId = devices[1].id;
+            //This is for cellphones with 4 cameras. Usually the first 2 detected are the front so in my case selecting the third in the list worked.
+            if (cameraLabel.includes("front")) {
+                cameraId = devices[2].id;
+            }
+        }
+
+        const html5QrCode = new Html5Qrcode("qr-reader");
+        html5QrCode.start(
+            cameraId,
+            {
+                fps: 10,
+                qrbox: 250
+            },
+            qrCodeMessage => {
+                //Things you want to do when you match a QR Code
+            },
+            errorMessage => {
+                // parse error, ignore it.
+            })
+            .catch(err => {
+                // Start failed, handle it.
+            });
+
+    }
+}).catch(err => {
+
+});
+
 var html5QrcodeScanner = new Html5QrcodeScanner(
     "qr-reader", { fps: 10, qrbox: 250 }, false
 );
 
-$(document).ready(function () {
-
-    Html5Qrcode.getCameras().then(devices => {
-        if (devices && devices.length) {
-            var cameraId;
-            var cameraLabel;
-            if (devices.length === 1) {
-                cameraId = devices[0].id;
-            } else {
-                cameraId = devices[1].id;
-                //This is for cellphones with 4 cameras. Usually the first 2 detected are the front so in my case selecting the third in the list worked.
-                if (cameraLabel.includes("front")) {
-                    cameraId = devices[2].id;
-                }
-            }
-
-            const html5QrCode = new Html5Qrcode("reader");
-            html5QrCode.start(
-                cameraId,
-                {
-                    fps: 10,
-                    qrbox: 250
-                },
-                qrCodeMessage => {
-                    //Things you want to do when you match a QR Code
-                },
-                errorMessage => {
-                    // parse error, ignore it.
-                })
-                .catch(err => {
-                    // Start failed, handle it.
-                });
-
-        }
-    }).catch(err => {
-    });
-});
-html5QrcodeScanner.render(onScanSuccess,onScanFailure);
+html5QrcodeScanner.render(Html5Qrcode,onScanSuccess,onScanFailure);
